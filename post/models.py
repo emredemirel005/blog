9da5f229ba6e1f16a21ba3497736c1ae89f4b577
emredemirel005 <emredemirel005@gmail.com>
2023-09-03 13:13:47 +0300

    .

--- post/models.py
+++ post/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
-from markdownx.models import MarkdownxField
+# from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
-
 class Post(models.Model):
     post_id = models.AutoField(auto_created=True,primary_key=True)
     author = models.CharField(max_length=30,null=False)
     title = models.CharField(max_length=200)
     intro = models.TextField()
-    body = models.TextField(MarkdownxField(name='Write your content here'))
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(null=True, blank=True,unique=True,db_index=True, editable=False)
 
